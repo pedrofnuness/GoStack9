24de73043c6e88a42426117de28e5335598ce7a8
Pedro Nunes <pedro9661@gmail.com>
2020-07-10 12:57:05 -0300

    ScheduleController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,7 @@ import UserController from "./app/controllers/UserController";
 import FileController from "./app/controllers/FileController";
 import SessionController from "./app/controllers/SessionController";
 import ProviderController from "./app/controllers/ProviderController";
+import ScheduleController from "./app/controllers/ScheduleController";
 import AppointmentController from "./app/controllers/AppointmentController";
 
 import authMiddleware from "./app/middlewares/auth";
@@ -24,6 +25,8 @@ routes.get("/providers", ProviderController.index);
 
 routes.post("/appointments", AppointmentController.store);
 routes.get("/appointments", AppointmentController.index);
+
+routes.get("/schedule", ScheduleController.index);
 
 routes.post("/files", upload.single("file"), FileController.store);
 
