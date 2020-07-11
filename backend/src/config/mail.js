019cfbe59d4ebfd3330c4736e3d11bcb136dc5f5
Pedro Nunes <pedro9661@gmail.com>
2020-07-11 14:37:12 -0300

    adding nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "09266d54abd740",
+    pass: "9ad3adaf8dee12",
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>",
+  },
+};
