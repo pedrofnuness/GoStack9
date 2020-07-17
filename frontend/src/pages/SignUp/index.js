f16974aabae637ac7d40bae2b562852d9b2c08a5
Pedro Nunes <pedro9661@gmail.com>
2020-07-17 14:39:32 -0300

    yup validator

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-export default function SignUp() {
-  return <div></div>;
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome obrigatório'),
+  email: Yup.string().email('E-mail inválido.').required('E-mail obrigatório!'),
+  password: Yup.string()
+    .min(6, 'Mínimo de 6 dígitos')
+    .required('Senha obrigatória!'),
+});
+
+function SignUp() {
+  function handleSubmit(data) {}
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já possuo login</Link>
+      </Form>
+    </>
+  );
 }
+
+export default SignUp;
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../pages/layouts/auth';
-import DefaultLayout from '../pages/layouts/default';
+import AuthLayout from '~/pages/layouts/auth';
+import DefaultLayout from '~/pages/layouts/default';
 
 export default function RouteWrapper({
   component: Component,
