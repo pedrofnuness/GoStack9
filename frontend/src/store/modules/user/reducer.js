991fdd8dcb646c41a2a37cc4e7534f1f43be9435
Pedro Nunes <pedro9661@gmail.com>
2020-07-18 16:15:27 -0300

    signOut func

--- frontend/src/store/modules/user/reducer.js
+++ frontend/src/store/modules/user/reducer.js
@@ -12,9 +12,13 @@ export default function user(state = INITIAL_STATE, action) {
 
         break;
       }
-
       case '@user/UPDATE_PROFILE_SUCCESS': {
         draft.profile = action.payload.profile;
+
+        break;
+      }
+      case '@auth/SIGN_OUT': {
+        draft.profile = null;
 
         break;
       }
