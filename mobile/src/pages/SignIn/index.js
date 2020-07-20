0c7ece2c4893142cca6508bcb67a0fe9502b27ce
Pedro Nunes <pedro9661@gmail.com>
2020-07-20 14:02:34 -0300

    Creating Background

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import Background from '~/components/Background';
+
+const SignIn = () => {
+  return (
+    <Background>
+      <Text>test</Text>
+    </Background>
+  );
+};
+
+export default SignIn;
---
+++ mobile/src/pages/SignUp/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import Background from '~/components/Background';
+// import { Container } from './styles';
+
+const SignUp = () => {
+  return (
+    <Background>
+      <Text>Gremio</Text>
+    </Background>
+  );
+};
+
+export default SignUp;
