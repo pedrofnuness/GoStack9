8c22c17c1ac70939104010a74a1c3dc30cb85100
Pedro Nunes <pedro9661@gmail.com>
2020-07-20 14:49:55 -0300

    created Button and Input components

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 const SignIn = () => {
   return (
     <Background>
       <Text>test</Text>
+
+      <Input
+        style={{marginTop: 30}}
+        icon="call"
+        placeholder="Digite alguma coisa"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 };
